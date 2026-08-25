4a6c1c7e901fc352d4ffea4b893aca8af9c747ba
Sai Zaw Myint <96133665+SaiZawMyint@users.noreply.github.com>
2026-08-26 00:01:42 +0630

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace icpms_client;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
